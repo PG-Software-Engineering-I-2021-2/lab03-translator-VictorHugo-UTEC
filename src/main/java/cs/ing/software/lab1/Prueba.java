0003f23617ea359b3d2a409874a516a13089605b
VICTOR HERRERA <victorherrera@VICTORs-MacBook-Pro.local>
2021-09-18 20:11:17 -0500

    Primera version con error 403

--- src/main/java/cs/ing/software/lab1/Prueba.java
+++ src/main/java/cs/ing/software/lab1/Prueba.java
@@ -1,28 +1,23 @@
 package cs.ing.software.lab1;
+import com.google.cloud.translate.*;
+
+
 import java.util.logging.Logger;
+
 
 public class Prueba {
 
     static final Logger logger = Logger.getLogger(Prueba.class.getName());
 
+    public static String traducir(String texto){
+        System.setProperty("GOOGLE_API_KEY", "8a254d832349fc50726bfeb26201bee1af230ad1");
+        Translate translate = TranslateOptions.getDefaultInstance().getService();
+        Translation translation = translate.translate(texto);
+        return translation.getTranslatedText();
+    }
+
     public static void main(String[] args)
     {
-        Automovil auto1 = new Automovil(0.9,100,10);
-        String auto1Viaje = auto1.imprimirViaje(4);
-        logger.info(auto1Viaje);
-        String auto1Viaje2 = auto1.reabastecer(20);
-        logger.info(auto1Viaje2);
-        String auto1Viaje3 = auto1.imprimirViaje(30);
-        logger.info(auto1Viaje3);
-        String auto1Viaje4 = auto1.reabastecer(200);
-        logger.info(auto1Viaje4);
-
-        Camion camion = new Camion(1.6,100,10);
-        String camionViaje1 = camion.imprimirViaje(4);
-        logger.info(camionViaje1);
-
-        String camionViaje2 = camion.reabastecer(50);
-        logger.info(camionViaje2);
-
+        logger.info(traducir("Hola Mundo"));
     }
 }
